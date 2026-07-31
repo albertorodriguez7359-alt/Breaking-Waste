bd7b7a9c492454252182017fdea00a387a1a7a14
Alberto <albertorodriguez7359@gmail.com>
2026-07-30 19:42:24 -0600

    Corrige diseño responsive del inicio de sesión y registro

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
     monitor serial muestre una IP diferente.
 */
 
-const ESP32 = "http://192.168.0.20";
+const ESP32 = "http://192.168.0.206";
 
 /*
     Ruta que utiliza el servidor web del ESP32.
